b6f8ab325d97ecdda866daf9b69028cc0bf9dcb4
Morulus <vladimirmorulus@gmail.com>
2015-02-09 04:44:21 +0300

    Поддерживается базовый функционал, производит сборку всех файлов из целевой директорию в боевую директорию. Шаблонный API поддерживает функцию snippet

---
+++ tasks/lib/snipper.js
@@ -0,0 +1,39 @@
+var fs = require('fs');
+var _ = require('underscore');
+var path = require('path');
+exports.init = function() {
+	return new (function() {
+	
+		this.render = function(source, build) {
+			var file = this.getFile(source);
+			fs.writeFileSync(build, file.render());
+			return true;
+		}
+
+		this.getFile = function(source) {
+			var hp = this;
+			return new (function(source) {
+				this.source = source;
+				this.dirname = path.dirname(source);
+				/* Render this file */
+				this.render = function() {
+					var fc = fs.readFileSync(this.source, 'utf8');
+					var templated = _.template(fc, {
+						hp: this,
+						_: _
+					});
+					return templated({
+						hp: this,
+						_: _
+					});
+				}
+				/* Render subfile */
+				this.snippet = function(filepath) {
+
+					var fh = hp.getFile(this.dirname+(filepath.substring(0,1)=='/' ? '':'/')+filepath);
+					return fh.render();
+				}
+			})(source);
+		};
+	})();
+}
