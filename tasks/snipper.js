9bb9a4175fcbb3441344c20ff657128a457d0a37
Morulus <vladimirmorulus@gmail.com>
2015-03-12 23:39:11 +0300

    up ver, upd from brahmajs

--- tasks/snipper.js
+++ tasks/snipper.js
@@ -1,7 +1,7 @@
 var path = require('path');
 module.exports = function( grunt ) {
 	 // Internal lib.
-  	var snipper = require('./lib/snipper.js').init(grunt);
+  	var uglify = require('./lib/snipper.js').init(grunt);
   	 grunt.registerMultiTask('snipper', 'The assembly of file fragments', function() {
   	 	var options = this.options({
 	      compress: false
@@ -13,7 +13,6 @@ module.exports = function( grunt ) {
 	      var src = f.src.filter(function (filepath) {
 
 	        // Warn on and remove invalid source files (if nonull was set).
-
 	        if (!grunt.file.exists(filepath)) {
 	          grunt.log.warn('Source file ' + chalk.cyan(filepath) + ' not found.');
 	          return false;
@@ -26,16 +25,9 @@ module.exports = function( grunt ) {
 	        grunt.log.warn('Destination ' + chalk.cyan(f.dest) + ' not written because src files were empty.');
 	        return;
 	      }
-
 	      src.forEach(function(file) {
 	         var fn = path.basename(file);
-	         var build = f.dest+fn;
-	         file = file.split("\\").join('/');
-	         build = build.split("\\").join('/');
-	         !/^[a-z]{1,4}:/i.test(file) && (file = process.cwd()+(file.substr(0,1)!=='/' ? '/' : '')+file); 
-	         !/^[a-z]{1,4}:/i.test(build) && (build = process.cwd()+(build.substr(0,1)!=='/' ? '/' : '')+build); 
-	         
-	         snipper.render(file, build);
+	         uglify.render(process.cwd()+'/'+file, process.cwd()+'/'+f.dest+fn);
 	      	 grunt.log.writeln('File '+file+' rendered to '+f.dest+fn);
 	      });
 	    
